3b50ab812df92362ca2e73c8959a557652f4d059
GR-plume <stellarvento@gmail.com>
2021-01-16 18:02:15 +0900

    initial

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import React, { useReducer } from 'react'
+import reducer from './reducer'
+
+const initialState = {
+}
+
+const Store = React.createContext()
+
+const Provider = ({children}) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+    return <Store.Provider value={{state, dispatch}}>{children}</Store.Provider>
+}
+
+export {Store, Provider}
